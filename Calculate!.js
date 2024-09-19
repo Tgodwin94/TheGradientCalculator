12b5439b2ed52200905f53f801bd37aeef928ffe
Tgodwin94 <tyler.godwin94@gmail.com>
2024-09-19 18:13:15 -0400

    Add files via upload

---
+++ Calculate!.js
@@ -0,0 +1,67 @@
+function click0() {
+    document.getElementById("area").innerHTML += "0"
+}
+
+function click1() {
+    document.getElementById("area").innerHTML += "1"
+}
+
+function click2() {
+    document.getElementById("area").innerHTML += "2"
+}
+
+function click3() {
+    document.getElementById("area").innerHTML += "3"
+}
+
+function click4() {
+    document.getElementById("area").innerHTML += "4"
+}
+
+function click5() {
+    document.getElementById("area").innerHTML += "5"
+}
+
+function click6() {
+    document.getElementById("area").innerHTML += "6"
+}
+
+function click7() {
+    document.getElementById("area").innerHTML += "7"
+}
+
+function click8() {
+    document.getElementById("area").innerHTML += "8"
+}
+
+function click9() {
+    document.getElementById("area").innerHTML += "9"
+}
+
+function clickAdd() {
+    document.getElementById("area").innerHTML += "+"
+}
+
+function clickSubtract() {
+    document.getElementById("area").innerHTML += "-"
+}
+
+function clickMultiply() {
+    document.getElementById("area").innerHTML += "*"
+}
+
+function clickDivide() {
+    document.getElementById("area").innerHTML += "/"
+}
+
+function clickDecimal() {
+    document.getElementById("area").innerHTML += "."
+}
+
+function clickEquals() {
+    document.getElementById("area").innerHTML = eval(document.getElementById("area").innerHTML)
+}
+
+function clickClear() {
+    document.getElementById("area").innerHTML = ""
+}
